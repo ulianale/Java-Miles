7f9393932bf8e7954f1801b10ed40100adc10e33
Uliana Le <ulianochka29@mail.ru>
2022-06-24 03:35:02 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 110; // стоимость билета
+        int one_mile = 20; // одна миля за 20 руб
+        System.out.println("Начислено миль: " + amount / one_mile);
+
+    }
+}
